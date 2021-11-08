c3035e5c54a6b3fb29d785e54311e10a04e28e12
Brandon Ching <bching9@gmail.com>
2021-11-07 23:55:47 -0700

    Update Temperature_Control_System.ino

--- Arduino Worksheet 4/Temperature_Control_System/Temperature_Control_System.ino
+++ Arduino Worksheet 4/Temperature_Control_System/Temperature_Control_System.ino
@@ -9,7 +9,7 @@ double tempK = 0.0; // initialize temp in Kelvin variable
 float tempC = 0.0; // initialize temp in Celcius variable
 float tempF = 0.0; // initialize temp in Fahrenheight variable
 long curtime = 0; // initialize sampling time tracking variable
-int threshold = 32; // initialize temp temperature threshold trigger variable, may need to adjust this for your hand temperature
+int threshold = 26; // initialize temp temperature threshold trigger variable, may need to adjust this for your hand temperature
 
 // Define pins for DC motor with fan using the L293D h-bridge motor controller
 int motorpin=5;     // initialize motor speed pin
@@ -34,11 +34,23 @@ void setup() {    // setup outputs, runs once when powered on
 void loop() { // runs continuously as long as the Arduino has power
   
   // Thermistor code here, use temperature in C (tempC) variable
-
-  // if statement which turns the motor on (set motor speed and a direction) and the servo angle to 90 degrees
-
-  // else statement which turns the motor off and the servo angle to 0 degrees
-   
+  tempReading = analogRead(tempPin);
+  tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );
+  tempC = tempK - 273.15;             // conversion from Kelvin to Celcius
+  tempF = (tempC * 9.0)/ 5.0 + 32.0;  // conversion from Kelvin to Fahrenheit
+  Serial.println(tempC);
+  
+  if (tempC >= threshold) {// if statement which turns the motor on (set motor speed and a direction) and the servo angle to 90 degrees
+    myservo.write(90);
+    digitalWrite(direction1, HIGH);
+    analogWrite(motorpin, 255);
+  } else {// else statement which turns the motor off and the servo angle to 0 degrees
+    myservo.write(0);
+    digitalWrite(direction1, LOW);
+    digitalWrite(direction2, LOW);
+    analogWrite(motorpin, 0);
+  }
    delay(200);  //short 200 ms delay to slow program down so the data collects slower and is more readable
    
 } // end void loop, repeat as long as Arduino has power
